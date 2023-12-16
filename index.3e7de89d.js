775a5541f2a4acbf2302800cc9c3d2ce82ed5177
Andreas-206 <141054318+Andreas-206@users.noreply.github.com>
2023-12-16 10:31:58 +0000

    Deploying to gh-pages from @ Andreas-206/notes-book-1@7974f76b11e08dd35a9fa3d3eb144a454c2e66c6 🚀

--- index.3e7de89d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={form:document.querySelector("#task-form")};e.form.addEventListener("submit",(function(r){r.preventDefault();var t={};new FormData(r.currentTarget).forEach((function(e,r){t[r]=e})),e.form.reset(),console.log(t)}))}();
-//# sourceMappingURL=index.3e7de89d.js.map
